be41c554a649ee351e300df8aa25e5367b585d4d
Shanmukha2001 <51831110+Shanmukha2001@users.noreply.github.com>
2021-01-10 20:22:54 +0530

    Add files via upload

---
+++ public/sample.js
@@ -0,0 +1,23 @@
+//PUBLIC FOLDER IS USED TO STORE ALL THE STATIC FILES SUCH AS IMAGES,JS FILES,CSS FILES( INSIDE CSS FOLDER) ETC
+
+// create additional js files inside this folder only and use these files in your client.ejs file 
+// you need not mention complete path.... say for ex: we create a file Hk.js inside the client.ejs we 
+// just need to use <script src="Hk.js" defer ></script> we usually give complete path but this is taken care by express in our project
+
+var socket = io();
+
+
+var form = document.getElementById('form'); // using form
+var input = document.getElementById('input'); // using input
+
+// the below code works when we type something in the input and click enter or click on submit button
+// submit is a keyword which is activated on submiting the form
+// we are also passing a call back function with e as parameter ( where e takes care of the form to avoid refreshing)
+// refer to this site https://www.w3schools.com/jsref/event_onsubmit.asp <= ctrl + click on the link to open the website directly 
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevent page to refresh
+    if (input.value) { // if input has some value
+        socket.emit('chat message', input.value); // send everyone that a message has arrived and send the message enterd
+        input.value = ''; // set input value as '' i.e remove all the characters entered by user and make it empty
+    }
+});
---
+++ server.js
@@ -0,0 +1,99 @@
+/* 
+
+NOTE: I SUGGEST YOU TO HAVE A GLANCE AT ALL THE WEBSITES PROVIDED..NO NEED TO ALL THE ANSWERS READ
+THE MOST RELEVANT AND EASY TO UNDERSTAND ANSWERS (WITH HIGH VOTES PROVIDED)
+
+https://youtu.be/xHLd36QoS4k <= see this video first to understand how require() works
+
+https://stackoverflow.com/questions/18234491/two-sets-of-parentheses-after-function-call
+https://stackoverflow.com/questions/24044997/what-does-double-parentheses-mean-in-a-require
+REFER THE ABOVE SITES TO UNDERSTAND WHY WE ARE USING DOUBLE PARENTHESES require()()
+
+prerequisite:
+    functions to arrow functions(used in ES6) =>https://www.youtube.com/watch?v=h33Srr5J9nY
+    basics of json in 10min => https://www.youtube.com/watch?v=iiADhChRriM
+    call back functions => https://www.w3schools.com/js/js_callback.asp ( try writing some code)
+
+
+*/
+
+const express = require('express') // importing express 
+const app = require('express')(); // importing express and storing the function in app
+const http = require('http').createServer(app); // importing http and passing app to create a server
+const { v4: uuidv4 } = require('uuid') // import v4 as uuidv4 from uuid, this is used to generate random length room numbers
+const port = '3000'; // creating a port variable
+let room = ''; // to store room id
+// https://www.youtube.com/watch?v=ggVsXljT0MI <= see this if you did not understand the above explanation
+// NOTE: THE ABOVE VIDEO IS THREE YEARS AGO( OUT DATED AND CANNOT BE USED IN PRESENT VERSION WHICH IS BEING USED) SO JUST UNDERSTAND IT 
+
+
+const io = require('socket.io')(http); // import socket io and pass http to it
+
+// set view engine as ejs since there were many errors associated with html
+// moreover html is static type and ejs( https://www.quora.com/Why-do-developers-have-to-use-Jade-and-EJS-instead-of-HTML-with-Express-and-Node ) is dynamic
+app.set('view engine', 'ejs') 
+app.use(express.static('public')) 
+// we are saying express to use public folder for all the static files, such as css,images and js
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`); 
+    // generate a random room code(uuidv4() => this generated a random room ) and redirect to the generated room 
+});
+
+// once a random room number is generated , we can use it by using 
+// /:room here 'room' is a variable name which is attached to req in the form of json see the below ex
+// {
+//     req{
+//          ...
+//          {
+//            params{
+//                  ....,
+//                  room: "some-random-room-id-generated",
+//                  ....
+//              }
+//          },
+//          ...
+//      }
+// }
+// you may see the req json code by using console.log(req) inside the below call back function
+app.get('/:room', (req, res) => {
+    res.render('client', { roomId: req.params.room });
+    // where roomId is the variable name which is passed to client.js file 
+
+    room = req.params.room;
+    // console.log(req); // uncomment this to see the json file and find it in the terminal ( ctrl+f works in terminal also)
+
+    // render( show the user the client.ejs file ) and send roomId
+    // we can use this roomId by typing <%= roomId %> inside the client.ejs file inside the script tags
+});
+
+/*  DO NOT CHANGE ABOVE CODE WRITE YOUR CODE INSIDE THIS BLOCK  
+*/
+ 
+io.on('connection', (socket) => {
+    console.log('a user connected'); // print this line when a user is connectd in the terminal as this is server side
+    console.log("room id: "+room); // print the room id
+
+    // when a user emits chat message( this can be anything as this is just a variable)
+    // in sample.js which is inside the public folder
+    // we are sending the text entered by user inside the input ... which is recieved as msg
+    // this msg is consoled in the server again
+
+    //CHECK OUT THE CHEAT SHEET => TO UNDERSTAND THIS MORE CLEARLY
+    // https://socket.io/docs/v3/emit-cheatsheet/
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+      });
+
+    // when a user disconnects 'disconnect' is triggered and shown in the terminal
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+});
+
+/*  DO NOT CHANGE BELOW CODE WRITE YOUR CODE INSIDE THIS BLOCK  */
+
+// listen the port, and when the server is ready...
+http.listen(port, () => {
+    console.log(`go to  locahost:${port}`) //... show this message in the cmd prompt
+});
